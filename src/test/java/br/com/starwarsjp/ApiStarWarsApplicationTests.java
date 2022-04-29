7bab88112c925a686d4ba804b9f5692e3f77a831
João Pedro Bueno <joaopedrobueno2910@gmail.com>
2022-04-29 16:18:18 -0300

    Initial commit

---
+++ src/test/java/br/com/starwarsjp/ApiStarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.starwarsjp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
